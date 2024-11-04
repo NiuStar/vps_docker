61727c0c08777aa5c45bd550e7531c161c4219f8
Niu Star <yjkj2@qq.com>
2024-11-04 10:21:20 +0800

    增加一键修改端口号和密码的命令

--- install.sh
+++ install.sh
@@ -9,18 +9,44 @@ else
     IP_FLAG="-4"
 fi
 
-echo "安装docker"
-curl $IP_FLAG -fsSL https://test.docker.com -o test-docker.sh \
-&& sudo sh test-docker.sh \
-&& sudo curl $IP_FLAG -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+echo "安装 Docker"
+curl $IP_FLAG -fsSL https://get.docker.com -o get-docker.sh \
+&& sudo sh get-docker.sh
+
+if [ $? -ne 0 ]; then
+    echo "Docker 安装失败"
+    exit 1
+fi
+
+echo "安装 Docker Compose"
+sudo curl $IP_FLAG -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
 && sudo chmod +x /usr/local/bin/docker-compose
 
-#echo "安装portainer"
-#docker run -d -p 19001:9000 -p 19002:9001 --restart=always -v /var/run/docker.sock:/var/run/docker.sock --name portainer portainer/portainer-ce
+if [ ! -f /usr/local/bin/docker-compose ]; then
+    echo "Docker Compose 安装失败"
+    exit 1
+fi
 
 echo "下载文件"
 wget $IP_FLAG https://github.com/NiuStar/vps_docker/raw/main/docker.zip
+if [ $? -ne 0 ]; then
+    echo "文件下载失败"
+    exit 1
+fi
+
 apt install -y unzip
 unzip docker.zip
 
+if [ ! -d "docker/x-ui" ]; then
+    echo "x-ui 目录不存在"
+    exit 1
+fi
+
 cd docker/x-ui && docker-compose up -d
+
+if [ $? -ne 0 ]; then
+    echo "Docker Compose 启动失败"
+    exit 1
+fi
+
+chmod +x ../sshport.sh && ../sshport.sh
