ad3f05f98ff0298bd6791c2b40a8539cbf8549d4
Niu Star <yjkj2@qq.com>
2024-11-04 09:58:37 +0800

    fix:脚本支持输入-6，以使用ipv6网络

--- install.sh
+++ install.sh
@@ -1,16 +1,26 @@
-#bash
-echo "安装docker"
- curl -fsSL https://test.docker.com -o test-docker.sh \
- && sudo sh test-docker.sh && sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
- 
- #echo "安装portainer"
- #docker run -d -p 19001:9000 -p 19002:9001 --restart=always -v /var/run/docker.sock:/var/run/docker.sock --name portainer portainer/portainer-ce
- 
+#!/bin/bash
 
- echo "下载文件"
-wget https://github.com/NiuStar/vps_docker/raw/main/docker.zip
-apt install unzip
+# 检查是否有参数传入
+if [ "$1" == "-6" ]; then
+    echo "选择了 IPv6 模式"
+    IP_FLAG="-6"
+else
+    echo "选择了 IPv4 模式"
+    IP_FLAG="-4"
+fi
+
+echo "安装docker"
+curl $IP_FLAG -fsSL https://test.docker.com -o test-docker.sh \
+&& sudo sh test-docker.sh \
+&& sudo curl $IP_FLAG -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+&& sudo chmod +x /usr/local/bin/docker-compose
+
+#echo "安装portainer"
+#docker run -d -p 19001:9000 -p 19002:9001 --restart=always -v /var/run/docker.sock:/var/run/docker.sock --name portainer portainer/portainer-ce
+
+echo "下载文件"
+wget $IP_FLAG https://github.com/NiuStar/vps_docker/raw/main/docker.zip
+apt install -y unzip
 unzip docker.zip
- 
+
 cd docker/x-ui && docker-compose up -d
- 
